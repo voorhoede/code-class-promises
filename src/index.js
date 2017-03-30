0c3dd643ec10fc62ee9889058da3d48259fe3a6b
Jurgen Beliën <jurgen@voorhoede.nl>
2017-03-30 16:44:58 +0200

    add placeholder function on startButton

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const output = document.getElementById('output');
 const startButton = document.getElementById('start');
 
-function logOutput(message) {
-    output.innerHTML = `${output.innerHTML}\n${message}`; 
+function logOutput (message) {
+  output.innerHTML = `${output.innerHTML}\n${message}`;
 }
 
-logOutput(`
-     _____  _____  _____  _______  __________  ______________  
-    /  __ |/  __ |/  _  |/       |/__/  __   |/      /  __   | 
-   /  /_/ /  /_/ /  //  /  /  /  /  /__   l__/  ----/__   l__/ 
-  /  ____/  __  |  //  /  /  /  /  /   l_   |  ----/   l_   |  
- /__/   /__/ /__/_____/__/__/__/__/|_______/______/|_______/  
-`);
+startButton.addEventListener('click', () => {}, false);
 
+logOutput(`
+     _____  _____  _____  _______  __________  ______________
+    /  __ |/  __ |/  _  |/       |/__/  __   |/      /  __   |
+   /  /_/ /  /_/ /  //  /  /  /  /  /__   l__/  ----/__   l__/
+  /  ____/  __  |  //  /  /  /  /  /   l_   |  ----/   l_   |
+ /__/   /__/ /__/_____/__/__/__/__/|_______/______/|_______/
+`);
